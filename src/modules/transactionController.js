7763196990cc85d7c3970074068581ddeebc6416
ramonivandy <ramonivandy21@gmail.com>
2024-12-09 20:57:35 +0700

    feat: transaction post

--- src/modules/transactionController.js
+++ src/modules/transactionController.js
@@ -57,33 +57,81 @@ const postTopup = async (req, res) => {
   ];
   const insertTransaction = await db.query(sql, params);
 
-  // update balance user
-  // get current balance
-  sql = "SELECT * FROM balances WHERE user_id = $1";
-  params = [req.decodedToken.id];
-  const userBalance = await db.query(sql, params);
-
-  // add balance
-  console.log(userBalance);
-
-  let newBalance =
-    Math.floor(userBalance.rows[0].balance) +
-    Math.floor(req.body.top_up_amount);
-
-  sql = "UPDATE balances SET balance = $1 WHERE user_id = $2 RETURNING balance";
-  params = [newBalance, req.decodedToken.id];
-  const updateBalance = await db.query(sql, params);
+  const updateBalanceUser = await updateBalance(
+    req.decodedToken.id,
+    "topup",
+    req.body.top_up_amount
+  );
 
   return res.json({
     status: 0,
     message: "Top Up Balance berhasil",
     data: {
-      balance: updateBalance.rows[0].balance,
+      balance: updateBalanceUser.balance,
     },
   });
 };
 
-const postTransaction = async (req, res) => {};
+const postTransaction = async (req, res) => {
+  const { service_code } = req.body;
+
+  //get service tarif
+  let sql =
+    "SELECT service_code, service_name, service_tarif FROM services WHERE service_code = $1";
+  let params = [service_code];
+  const serviceData = await db.query(sql, params);
+
+  if (serviceData.rows.length < 1) {
+    return res.status(400).json({
+      status: 102,
+      message: "Service atau layanan tidak ditemukan",
+      data: null,
+    });
+  }
+
+  //get user balance
+  sql = "SELECT balance FROM balances WHERE user_id = $1";
+  params = [req.decodedToken.id];
+  let balanceUser = await db.query(sql, params);
+
+  //check if balance enough to make transaction
+  balanceUser = Math.floor(balanceUser.rows[0].balance);
+  serviceCost = Math.floor(serviceData.rows[0].service_tarif);
+
+  if (balanceUser < serviceCost) {
+    return res.status(402).json({
+      status: 402,
+      message: "Balance tidak mencukupi",
+      data: null,
+    });
+  }
+
+  // insert transaction
+  sql =
+    "INSERT INTO transactions (user_id, invoice_number, service_code, service_name, transaction_type, total_amount) VALUES ($1, $2, $3, $4, $5, $6) RETURNING invoice_number, service_code, service_name, transaction_type, total_amount, created_on";
+  params = [
+    req.decodedToken.id,
+    generateInvoice(),
+    serviceData.rows[0].service_code,
+    serviceData.rows[0].service_name,
+    "PAYMENT",
+    serviceCost,
+  ];
+  const insertTransaction = await db.query(sql, params);
+
+  // update balance user
+  const updateBalanceUser = await updateBalance(
+    req.decodedToken.id,
+    "cost",
+    serviceCost
+  );
+
+  return res.status(200).json({
+    status: 0,
+    message: "Transaksi berhasil",
+    data: insertTransaction.rows,
+  });
+};
 
 function generateInvoice() {
   const prefix = "INV";
@@ -91,6 +139,26 @@ function generateInvoice() {
     .toString()
     .padStart(6, "0"); // Ensures 6 digits
   return (prefix + randomPart).substring(0, 10); // Ensures max length 10
+}
+
+async function updateBalance(userId, type, amount) {
+  // get current balance
+  sql = "SELECT * FROM balances WHERE user_id = $1";
+  params = [userId];
+  const userBalance = await db.query(sql, params);
+
+  let newBalance = 0;
+  if (type === "topup") {
+    newBalance = Math.floor(userBalance.rows[0].balance) + Math.floor(amount);
+  } else {
+    newBalance = Math.floor(userBalance.rows[0].balance) - Math.floor(amount);
+  }
+
+  sql = "UPDATE balances SET balance = $1 WHERE user_id = $2 RETURNING balance";
+  params = [newBalance, userId];
+  const updateBalance = await db.query(sql, params);
+
+  return updateBalance.rows[0];
 }
 
 module.exports = {
