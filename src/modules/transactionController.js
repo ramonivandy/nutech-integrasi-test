83b7a85573334d35c50c1598bbfe3d3fa2b6cd2e
ramonivandy <ramonivandy21@gmail.com>
2024-12-09 19:56:30 +0700

    feat: transaction top up balance

--- src/modules/transactionController.js
+++ src/modules/transactionController.js
@@ -25,8 +25,54 @@ const getBalance = async (req, res) => {
 };
 
 const getTransactionHistory = async (req, res) => {};
-const postTopup = async (req, res) => {};
+const postTopup = async (req, res) => {
+  // create transaction
+  let sql =
+    "INSERT INTO transactions (user_id, invoice_number, service_code, service_name, transaction_type, total_amount) VALUES ($1, $2, $3, $4, $5, $6)";
+  let params = [
+    req.decodedToken.id,
+    generateInvoice(),
+    "TOPUP",
+    "TOPUP",
+    "TOPUP",
+    req.body.top_up_amount,
+  ];
+  const insertTransaction = await db.query(sql, params);
+
+  // update balance user
+  // get current balance
+  sql = "SELECT * FROM balances WHERE user_id = $1";
+  params = [req.decodedToken.id];
+  const userBalance = await db.query(sql, params);
+
+  // add balance
+  console.log(userBalance);
+
+  let newBalance =
+    Math.floor(userBalance.rows[0].balance) +
+    Math.floor(req.body.top_up_amount);
+
+  sql = "UPDATE balances SET balance = $1 WHERE user_id = $2 RETURNING balance";
+  params = [newBalance, req.decodedToken.id];
+  const updateBalance = await db.query(sql, params);
+
+  return res.json({
+    status: 0,
+    message: "Top Up Balance berhasil",
+    data: {
+      balance: updateBalance.rows[0].balance,
+    },
+  });
+};
 const postTransaction = async (req, res) => {};
+
+function generateInvoice() {
+  const prefix = "INV";
+  const randomPart = Math.floor(Math.random() * 1000000)
+    .toString()
+    .padStart(6, "0"); // Ensures 6 digits
+  return (prefix + randomPart).substring(0, 10); // Ensures max length 10
+}
 
 module.exports = {
   getBalance,
